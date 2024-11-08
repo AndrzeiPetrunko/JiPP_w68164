fb63df89d2ca9bf6d0fdc617fc7f0510ec212dbc
Andrii Petrunko <w68164@student.wsiz.edu.pl>
2024-11-08 20:32:11 +0100

    comit1

---
+++ zad2.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+def BFS(graph, start, end):
+    visited = set()
+    queue = deque([[start]])
+
+    while queue:
+        path = queue.popleft()
+        node = path[-1]
+        if node == end:
+            return path
+        if node not in visited:
+            for neighbor in graph.get(node, []):
+                new_path = list(path)
+                new_path.append(neighbor)
+                queue.append(new_path)
+            visited.add(node)
+    return None
+
+graph = {'A':['B'],
+         'B':['A', 'C', 'D'],
+         'C':['B', 'E'],
+         'D':['B', 'F'],
+         'E':['C', 'J', 'F'],
+         'F':['E', 'D'],
+         'J':['E']}
+
+print(BFS(graph, 'A', 'F'))
